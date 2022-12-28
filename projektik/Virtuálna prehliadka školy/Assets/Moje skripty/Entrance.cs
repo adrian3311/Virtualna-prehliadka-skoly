c43979bc1c24119e3cc4d1ef17bc21d4f64d8887
adrian3311 <adoliba33@gmail.com>
2022-12-28 16:19:14 +0100

    unity

--- projektik/Virtuálna prehliadka školy/Assets/Moje skripty/Entrance.cs
+++ projektik/Virtuálna prehliadka školy/Assets/Moje skripty/Entrance.cs
@@ -9,9 +9,7 @@ public class Entrance : MonoBehaviour
     public string SceneName;
 
 
-    private void OnMouseDown()
-    {
-        global::System.Object p = SceneManager.LoadScene(SceneName);
+    private void OnMouseDown() => SceneManager.LoadScene(SceneName);
 
-    }
+
 }
