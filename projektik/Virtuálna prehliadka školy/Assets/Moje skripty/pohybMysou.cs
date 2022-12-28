63cffc3455e1f305f68ea7c2ac3a4aaea977e3be
adrian3311 <adoliba33@gmail.com>
2022-12-28 23:03:12 +0100

    unity prechody

--- projektik/Virtuálna prehliadka školy/Assets/Moje skripty/pohybMysou.cs
+++ projektik/Virtuálna prehliadka školy/Assets/Moje skripty/pohybMysou.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class pohybMysou : MonoBehaviour
 {
+
+    public float speed = 2;
+
+    private Camera mainCamera;
+
+    [SerializeField]
+    private float minFov = 25, maxFov = 60;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        mainCamera = Camera.main;
+
     }
 
     // Update is called once per frame
@@ -17,10 +27,15 @@ public class pohybMysou : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
 
-            transform.Rotate(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+            transform.Rotate(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0);
+            var x = transform.rotation.eulerAngles.x;
+            var y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(x, y, 0);
 
         }
 
-
+        var delta = Input.mouseScrollDelta;
+        mainCamera.fieldOfView -= delta.y;
+        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, minFov, maxFov);
     }
 }
