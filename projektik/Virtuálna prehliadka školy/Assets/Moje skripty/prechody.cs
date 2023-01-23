bf2d8c26ee812144972bc34f3634b476017b3150
adrian3311 <75687565+adrian3311@users.noreply.github.com>
2023-01-23 17:51:59 +0100

    nove sceny

--- projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
+++ projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class prechody : MonoBehaviour
 {
 
     public string SceneName;
-
+    public int cameraRotation = 0;
 
 private void OnMouseDown()
 {
-
+    
     SceneManager.LoadScene(SceneName);
 
 }
