e7a599ce0fffed12c660543212fa5b571d84c0a2
adrian3311 <75687565+adrian3311@users.noreply.github.com>
2023-01-25 22:07:48 +0100

    logoPo30

--- projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
+++ projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
@@ -10,10 +10,19 @@ public class prechody : MonoBehaviour
     public string SceneName;
     public int cameraRotation = 0;
 
+    /*void Update()
+    {
+        int readText = File.ReadAllText("C:\\Users\\adoli\\Desktop\\Virtualna-prehliadka-skoly\\projektik\\Virtuálna prehliadka školy\\Assets\\data.txt");
+        this.rotate = new Vector3(0, int.Parse(readText), 0);
+    }*/
+
 private void OnMouseDown()
 {
-    
-    SceneManager.LoadScene(SceneName);
+        //string readText = File.ReadAllText("C:\\Users\\adoli\\Desktop\\Virtualna-prehliadka-skoly\\projektik\\Virtuálna prehliadka školy\\Assets\\data.txt");
+        //File.WriteAllText("C:\\Users\\adoli\\Desktop\\Virtualna-prehliadka-skoly\\projektik\\Virtuálna prehliadka školy\\Assets\\data.txt",cameraRotation.ToString());
+        //Debug.Log(readText);
+
+        SceneManager.LoadScene(SceneName);
 
 }
 
