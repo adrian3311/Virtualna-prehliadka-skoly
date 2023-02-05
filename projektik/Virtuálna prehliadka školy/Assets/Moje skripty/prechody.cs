2313b6c5c3b6fc20f15549ca808fee520ba10081
abil3311 <adoliba33@gmail.com>
2023-02-05 21:26:29 +0100

    animacia + web

--- projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
+++ projektik/Virtuálna prehliadka školy/Assets/Moje skripty/prechody.cs
@@ -10,11 +10,26 @@ public class prechody : MonoBehaviour
     public string SceneName;
     public int cameraRotation = 0;
 
+    Animator transition;
+
 private void OnMouseDown()
     {
         GameObject.Find("Manager").GetComponent<Manager>().uhol = cameraRotation;
 
+        transition = GameObject.Find("Animacia").GetComponent<Animator>();
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+
+    {
+
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
         SceneManager.LoadScene(SceneName);
+
 
     }
 }
